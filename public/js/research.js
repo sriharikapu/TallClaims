42e83267c616ff4a82dc87007e40a8b8c220b3c7
Ujjwal Agrawal <ujjwal.iitism@gmail.com>
2018-08-12 15:13:00 +0530

    Fixed conflicts

--- public/js/research.js
+++ public/js/research.js
@@ -3,17 +3,19 @@ $.get("http://localhost:5000/getList", function(data){
   data = data.research;
   toShow = "";
   for(var x = 0; x < data.length; x += 3){
-    temp = `<div class="row">`;
+    temp = `<div class="row" style="width: 100%">`;
     for(var y = 0; y < 3 && x+y < data.length; y++){
-      temp += `<div class="card col-sm" style="width: 18rem; margin-left: 20px;>
+      temp += `<div class="card col-sm" onclick=paperDetails("${data[x+y].id}")  style="width: 18rem; margin-left: 20px;">
               <div class="card-body">
                 <h5 class="card-title">${data[x+y].title}</h5>
                 <p class="card-text">${data[x+y].description}</p>
               </div>
 
               <div class="card-body">
-                <a onclick=paperDetails("${data[x+y].id}") class="card-link">3 Approve</a>
-                <a onclick=paperDetails("${data[x+y].id}") class="card-link">4 Unapprove</a>
+                <!--<aclass="card-link">3 Approve</a>-->
+                <!--<a onclick=paperDetails("${data[x+y].id}") class="card-link">4 Unapprove</a>-->
+                <a class="card-link">3 Approve</a>
+                <a class="card-link">4 Unapprove</a>
               </div>
             </div>`;
     }
