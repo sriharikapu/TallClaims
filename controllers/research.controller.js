9291655b7cc7fd9fb57eb16b379a47dd71286698
Nakul Havelia <nakulhavelia@gmail.com>
2018-08-12 11:57:52 +0530

    working

--- controllers/research.controller.js
+++ controllers/research.controller.js
@@ -3,19 +3,21 @@ var Datastore = require('nedb');
 researches = new Datastore({ filename: 'data/researches.db', autoload: true });
 module.exports = {
   find: function(req, res){
+    console.log("finding all");
     viewResearches = [];
-    researchModel.find({_id: { $exists: true}}, function(err, researches) {
+    researches.find({_id: { $exists: true}}, function(err, r) {
       if(err) {
         console.log(err);
       }
-      if(researches){
-        for(var x = 0; x <= researches.length; x++){
+      if(r){
+        console.log(r);
+        for(var x = 0; x < r.length; x++){
           viewResearches.push({
-            title: researches[x].title,
-            description: researches[x].description,
-            assigned: researches[x].assigned,
-            needed: researches[x].needed,
-            id: researches[x]._id
+            title: r[x].title,
+            description: r[x].description,
+            assigned: r[x].assigned,
+            needed: r[x].needed,
+            id: r[x]._id
           });
         }
         res.json({
@@ -28,7 +30,7 @@ module.exports = {
     address = req.body.address;
     to = req.body.to;
     verdict = "Pending";
-    researchModel.update({"_id": mongoose.Types.ObjectId(to)}, { $push: { "assigned": address, "verdict": verdict}}, function(err){
+    researches.update({"_id": to}, { $push: { assigned: { "assigned": address, "verdict": verdict}}}, function(err){
       if(err){
         console.log(err);
         res.json({
@@ -43,9 +45,23 @@ module.exports = {
     });
   },
   add: function(req, res){
+    console.log("adding a new");
     title = req.body.title;
+    address = req.body.address;
     description = req.body.description;
     assigned = [];
     needed = 3;
+    researches.insert({'title': title, 'description': description, 'assigned': assigned, 'needed': needed, 'address': address}, function (err, newDoc) {
+      if(err){
+        res.json({
+          'done': false
+        });
+      }
+      else{
+        res.json({
+          'done': true
+        });
+      }
+    });
   }
 };
