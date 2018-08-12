e149806005c9ed3bd1ae0aaf2c25d9c6b63fbb47
Nakul Havelia <nakulhavelia@gmail.com>
2018-08-12 15:13:18 +0530

    improve details page

--- public/js/researchDetails.js
+++ public/js/researchDetails.js
@@ -3,10 +3,13 @@ console.log("research details.js");
 $.get("http://localhost:5000/comments/" + localStorage.getItem('id'), function(data){
   console.log(data);
   comments = "";
-  for(x = 0; x < data.doc.assigned; x++){
+  for(x = 0; x < data.doc.assigned.length; x++){
     if(data.doc.assigned[x].comment){
       comments += `<li class="list-group-item">${data.doc.assigned[x].comment}</li>`;
     }
   }
+  console.log(comments);
   $('#comments').html(comments);
+  $('#title').html(data.doc.title);
+  $('#disabledInput2').html(data.doc.description);
 });
